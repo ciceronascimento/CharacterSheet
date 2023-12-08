3398e02b7e656d1b4ab1560b3740165ecf5f57e4
Cicero Nascimento <antoniociceero@gmail.com>
2023-12-08 13:31:18 -0300

    Create TabView [x] CatViewController [x] DogViewController [x] InfoViewController

---
+++ CharacterSheet/CatsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CatsViewController.swift
+//  CharacterSheet
+//
+//  Created by Cicero Nascimento on 08/12/23.
+//
+
+import UIKit
+
+class CatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+    }
+}
---
+++ CharacterSheet/DogsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DogsViewController.swift
+//  CharacterSheet
+//
+//  Created by Cicero Nascimento on 08/12/23.
+//
+
+import UIKit
+
+class DogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
